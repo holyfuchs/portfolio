1f51ed60529b960a55187d51e3b3c80ba061a86f
Patrick Fuchs <email@patrick-fuchs.com>
2025-08-16 23:31:24 +0200

    update description

--- public/locations/resize.sh
+++ public/locations/resize.sh
@@ -2,6 +2,8 @@ for file in *; do
   # Only process files that are images (jpg, jpeg, png)
   if [[ "$file" =~ \.(jpg|jpeg|png)$ ]]; then
     # Resize image to max 1024px width or height, keeping aspect ratio
-    mogrify -resize '303x202>' "$file"
+    mogrify -resize '707x471>' "$file"
   fi
 done
+
+# 2121 × 1414
